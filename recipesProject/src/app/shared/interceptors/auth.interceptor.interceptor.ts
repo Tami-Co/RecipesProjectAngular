563cdb6b0cebc9001102258c738390ec8a44d2e6
Tami-Co <158340919+Tami-Co@users.noreply.github.com>
2024-05-21 23:00:01 +0300

    added interceptor

---
+++ recipesProject/src/app/shared/interceptors/auth.interceptor.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { UserService } from '../services/user.service';
+
+export const authInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+  const userService=inject(UserService);
+  const token=userService.token;
+  if(token)
+    {
+      req=req.clone({
+        setHeaders:{Authorization:`Bearer ${token}`},
+      })
+    }
+  return next(req);
+};
